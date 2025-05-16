6d442d268476a70c93e027fe9864bd4d8672137d
Lanijil <jilaniless@gmail.com>
2025-05-16 12:31:14 +0200

    EN -> FR + changement admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,16 +8,16 @@ exports.register = async (req, res) => {
 
     try {
         const existingUser = await User.findOne({ username });
-        if (existingUser) return res.status(400).json({ message: 'User already exists' });
+        if (existingUser) return res.status(400).json({ message: 'Utilisateur déjà existant' });
 
         const hashedPassword = bcrypt.hashSync(password, 10);
         const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
 
-        res.status(201).json({ message: 'User registered' });
+        res.status(201).json({ message: 'Utilisateur enregistré' });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: 'Server error' });
+        res.status(500).json({ message: 'Erreur serveur' });
     }
 };
 
@@ -26,16 +26,16 @@ exports.login = async (req, res) => {
 
     try {
         const user = await User.findOne({ username });
-        if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+        if (!user) return res.status(400).json({ message: 'Mauvaise authentification' });
 
         const isMatch = bcrypt.compareSync(password, user.password);
-        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+        if (!isMatch) return res.status(400).json({ message: 'Mauvaise authentification' });
 
         const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         res.json({ token });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: 'Server error' });
+        res.status(500).json({ message: 'Erreur serveur' });
     }
 };
