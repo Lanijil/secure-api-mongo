0b0035fe0d9e9e1e4ae2d68a6d4541da320badbe
Lanijil <jilaniless@gmail.com>
2025-05-16 15:11:27 +0200

    Ajout User et users

--- models/User.js
+++ models/User.js
@@ -5,4 +5,6 @@ const UserSchema = new mongoose.Schema({
     password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
+
